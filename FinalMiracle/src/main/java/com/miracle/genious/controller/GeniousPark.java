653684b8f1a412da10c8c6e704e1dee0f6af07c4
user1 <user1@aclass15>
2017-11-28 17:50:18 +0900

    g

--- FinalMiracle/src/main/java/com/miracle/genious/controller/GeniousPark.java
+++ FinalMiracle/src/main/java/com/miracle/genious/controller/GeniousPark.java
@@ -3,5 +3,6 @@ package com.miracle.genious.controller;
 public class GeniousPark {
 	public static void main(String[] args) {
 		System.out.println("바본디 ㅎㅎ");
+		System.out.println("바보맞음 ㅎㅎㅎ");
 	}
 }
