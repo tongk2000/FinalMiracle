85488a9dba97a4734f117eea7293c1947e12ceff
user1 <user1@aclass15>
2017-11-28 17:17:58 +0900

    모름

--- FinalMiracle/src/main/java/com/miracle/genious/controller/GeniousPark.java
+++ FinalMiracle/src/main/java/com/miracle/genious/controller/GeniousPark.java
@@ -2,6 +2,6 @@ package com.miracle.genious.controller;
 
 public class GeniousPark {
 	public static void main(String[] args) {
-		System.out.println("Git 써야 하나요?");
+		System.out.println("바본디 ㅎㅎ");
 	}
 }
