12ba24f3d3651864dbf43ea17bbe859bcab9ef42
User <jsw4989@gmail.com>
2017-11-29 20:24:08 +0900

    출퇴근 체크 조금

--- FinalMiracle/src/main/java/com/miracle/jsw/controller/CommuteController.java
+++ FinalMiracle/src/main/java/com/miracle/jsw/controller/CommuteController.java
@@ -1,5 +1,10 @@
 package com.miracle.jsw.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -13,15 +18,62 @@ import com.miracle.jsw.service.InterCommuteService;
 
 public class CommuteController {
 	
-	//@Autowired
-	//private InterCommuteService service;
+	@Autowired
+	private InterCommuteService service;
 	
 	@RequestMapping(value="/commute.mr", method={RequestMethod.GET})
-	public String commute(){
+	public String commute(HttpServletRequest req){
 		
-		
+		List<HashMap<String, String>> commuteList = service.commuteList();
+		req.setAttribute("commuteList", commuteList);
 		
 		return "jsw/commute.all";
+	}
+	
+	@RequestMapping(value="/commutestart.mr", method={RequestMethod.GET})
+	public String commuteStart(HttpServletRequest req){
+		
+		int n = service.startWork();
+		
+		if (n>0) {
+			String msg = "출근체크 하였습니다";
+			String loc = "jsw/commute.all";
+			
+			req.setAttribute("msg", msg);
+			req.setAttribute("loc", loc);
+		}
+		else{
+			String msg = "출근체크 실패하였습니다";
+			String loc = "commute.mr";
+			
+			req.setAttribute("msg", msg);
+			req.setAttribute("loc", loc);
+		}
+		
+		return "jsw/msg.not";
+	}
+	
+	@RequestMapping(value="/commuteend.mr", method={RequestMethod.GET})
+	public String commuteEnd(HttpServletRequest req){
+		
+		int n = service.endWork();
+		
+		if (n>0) {
+			String msg = "퇴근체크 하였습니다";
+			String loc = "commute.mr";
+			
+			req.setAttribute("msg", msg);
+			req.setAttribute("loc", loc);
+		}
+		else{
+			String msg = "퇴근체크 실패하였습니다";
+			String loc = "jsw/commute.all";
+			
+			req.setAttribute("msg", msg);
+			req.setAttribute("loc", loc);
+		}
+		
+		return "jsw/msg.not";
 	}
 	
 	
