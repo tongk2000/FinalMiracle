9eeb704a338ab1472a1e23a0424dd5cffac668ba
user1 <user1@aclass15>
2017-11-30 15:02:48 +0900

    JSON 라이브러리 추가

--- FinalMiracle/src/main/java/com/miracle/kdh/service/ScheduleManagerService.java
+++ FinalMiracle/src/main/java/com/miracle/kdh/service/ScheduleManagerService.java
@@ -1,11 +1,14 @@
 package com.miracle.kdh.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.miracle.kdh.model.FolderVO;
+import com.miracle.kdh.model.Folder_CommentVO;
+import com.miracle.kdh.model.Folder_TeamwonVO;
 import com.miracle.kdh.model.ScheduleManagerDAO;
 
 @Service
@@ -18,4 +21,23 @@ public class ScheduleManagerService {
 		List<FolderVO> doList = dao.getAllDoList();
 		return doList;
 	} // end of List<FolderVO> getAllDoList() ------------------------------------------
+	
+	// 선택한 폴더의 모든 정보를 가져오기
+	public HashMap<String, Object> getSelectFolderInfo(String idx) {
+		// 선택한 폴더의 정보를 가져옴
+		FolderVO fvo = dao.getFolderInfo(idx);
+		
+		// 선택한 폴더에 소속된 팀원 리스트를 가져옴
+		List<Folder_TeamwonVO> folder_teamwonList = dao.getFolder_teamwonInfo(idx);
+		
+		// 선택한 폴더에 작성된 댓글 리스트를 가져옴
+		List<Folder_CommentVO> folder_commentList = dao.getFolder_commentInfo(idx);
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();  
+		map.put("fvo", fvo);
+		map.put("folder_teamwonList", folder_teamwonList);
+		map.put("folder_commentList", folder_commentList);
+		
+		return map;
+	} // end of HashMap<String, Object> getSelectFolderInfo(String idx) ------------------------------------------ 
 }
