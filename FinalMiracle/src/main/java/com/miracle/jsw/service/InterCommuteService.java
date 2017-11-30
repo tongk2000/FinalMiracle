f8a4739ed36c7ede9414a8a34bc478553519f74f
User <jsw4989@gmail.com>
2017-11-30 16:58:15 +0900

    팀장권한 출퇴근 내역보기

--- FinalMiracle/src/main/java/com/miracle/jsw/service/InterCommuteService.java
+++ FinalMiracle/src/main/java/com/miracle/jsw/service/InterCommuteService.java
@@ -16,5 +16,15 @@ public interface InterCommuteService {
 	int getTotalCountMonth(HashMap<String, String> map); // 총 페이지수
 
 	int getTotalCount(); // 총 페이지수
+
+	List<HashMap<String, String>> getTeamWonList(HashMap<String, String> map); // 팀장이 팀원들의 정보를 불러오는 메소드
+
+	List<HashMap<String, String>> twcommuteListMonth(HashMap<String, String> map); // 팀원의 출퇴근 리스트를 불러옴(월별 검색)
+
+	int getTWTotalCountMonth(HashMap<String, String> map); // 총 페이지수
+
+	List<HashMap<String, String>> twcommuteList(HashMap<String, String> map); // 팀원의 출퇴근 리스트를 불러옴
+
+	int getTWTotalCount(HashMap<String, String> map); // 총 페이지수
 	
 }
