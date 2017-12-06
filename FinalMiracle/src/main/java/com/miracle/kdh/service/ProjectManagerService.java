ddd88a61537478595ccfdda47f22e38c3665e335
user1 <user1@aclass15>
2017-12-06 22:04:22 +0900

    Merge branch 'master' of https://github.com/tongk2000/FinalMiracle.git
    Conflicts:
    	FinalMiracle/target/m2e-wtp/web-resources/META-INF/maven/com.miracle/miracle/pom.properties

--- FinalMiracle/src/main/java/com/miracle/kdh/service/ProjectManagerService.java
+++ FinalMiracle/src/main/java/com/miracle/kdh/service/ProjectManagerService.java
@@ -3,8 +3,6 @@ package com.miracle.kdh.service;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -61,21 +59,33 @@ public class ProjectManagerService {
 	public HashMap<String, String> getUpFolder(String upIdx) {
 		HashMap<String, String> map = dao.getUpFolder(upIdx);
 		return map;
-	} // public HashMap<String, String> getUpFolder(int upIdx) --------------------------------------------------------
+	} // end of HashMap<String, String> getUpFolder(int upIdx) --------------------------------------------------------
 
+	// 현재 팀의 소속된 팀원 목록을 가져오기
 	public List<HashMap<String, String>> getTeamwonList(String team_idx) {
 		List<HashMap<String, String>> teamwonList = dao.getTeamwonList(team_idx);
 		return teamwonList;
-	}
+	} // end of List<HashMap<String, String>> getTeamwonList(String team_idx) --------------------------------------------------
 
+	// 하위폴더 추가하기(+추가된 폴더에 소속된 담당들도 folder_teamwon 테이블에 추가)
 	@Transactional(propagation=Propagation.REQUIRED, isolation=Isolation.READ_COMMITTED, rollbackFor={Throwable.class})
-	public int addDownFolderEnd(FolderVO fvo, HashMap<String, Object> map) {
-		int result1 = dao.addDownFolder(fvo);
-		int folderIdx = dao.getLastest_FolderIdx();
-		map.put("folderIdx", folderIdx);
-		int result2 = dao.addFolderTeamwon(map);
-		return result1 * result2;
-	}
+	public HashMap<String, Object> addDownElementEnd(FolderVO fvo, HashMap<String, Object> map) {
+		int result1 = dao.addDownElement(fvo); // 하위 폴더 추가하기
+		int result2 = dao.addDoTeamwon(map); // 폴더나 할일 추가할때 담당 팀원 추가하기(가장 최근에 올라온 folderIdx를 구해서 입력주는 방식임)
+		fvo = dao.getAddedElement(); // 방금 추가한 요소를 가져오기
+		
+		HashMap<String, Object> returnMap = new HashMap<String, Object>();
+		returnMap.put("result", result1*result2);
+		returnMap.put("fvo", fvo);
+		
+		return returnMap;
+	} // end of int addDownElementEnd(FolderVO fvo, HashMap<String, Object> map) -----------------------------------------------------
+
+	// 선택한 요소와 그 하위요소들 삭제하기
+	public int delElement(String idx) {
+		int result = dao.delElement(idx);
+		return result;
+	} // end of int delElement(String idx) ----------------------------------------------------------------------------------------
 }
 
 
